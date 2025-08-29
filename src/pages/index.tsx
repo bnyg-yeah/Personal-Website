87af40433cb62440af11de3f01230faa48426355
Brighton <bnyg@vt.edu>
2025-08-29 01:57:22 -0400

    i didnt like it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <Layout
       title="Brighton Young .dev"
       subtitle="Explore my work and projects"
-      background={<VideoBackground />}
+      // background={<VideoBackground />}
     />
   );
 }
